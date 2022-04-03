ec9d894d8fe5eca4661f66fa2ebe7857378d8d41
onceup <31176746+onceup@users.noreply.github.com>
2022-04-03 17:09:35 +0300

    setted up elsint with ts

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ const router = express.Router();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-router.get('/', function (req, res) {
+router.get('/', function (_req: any, res: { sendFile: (string: any) => void; }) {
   res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
-app.listen(process.env.port || 3000, () => 'Server has started');
+app.listen(process.env.port || 3000, () => console.log('Server has started'));
